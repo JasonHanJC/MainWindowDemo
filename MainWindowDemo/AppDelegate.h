688095a9a0cd9b6c6ed9281fb34caf982c534141
Juncheng Han <namrie1990@gmail.com>
2016-09-20 12:25:26 -0700

    Initial Commit

---
+++ MainWindowDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MainWindowDemo
+//
+//  Created by Juncheng Han on 9/20/16.
+//  Copyright © 2016 Theohzoneinc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MainWindowDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MainWindowDemo
+//
+//  Created by Juncheng Han on 9/20/16.
+//  Copyright © 2016 Theohzoneinc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
